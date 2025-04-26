4ff12b8afbe022229f78b3fb9d505648782f2a1a
henrySilverIX <henrysilver.gold@proton.me>
2025-04-25 23:20:42 -0300

    feat: Adicionei os estados do Brasil no jogo.

--- assets/writerManager.py
+++ assets/writerManager.py
@@ -2,18 +2,23 @@ import pandas
 from turtle import Turtle
 import os
 
-#Utilização do pacote os para guardar a informação do caminho absoluto da pasta do projeto
+# Utilização do pacote os para guardar a informação do caminho absoluto da pasta do projeto
 # Caminho absoluto baseado na localização do script
 base_path = os.path.dirname(__file__)
-csv_path = os.path.join(base_path, '..', 'public', 'data', '50_states.csv')
 
 ALIGNMENT = "center"
 CLASS_FAMILY = "Montserrat"
 
 
 class GeographyManager(Turtle):
-    def __init__(self):
+    def __init__(self, country):
         super().__init__()
+        self.country = country
+        if country == 'Usa':
+            csv_path = os.path.join(base_path, '..', 'public', 'data', '50_states.csv')
+        elif country == "Brasil":
+            csv_path = os.path.join(base_path, '..', 'public', 'data', '26_states_brazil.csv')
+
         self.geographyDataFrame = pandas.read_csv(csv_path)
         self.column_state_list = self.geographyDataFrame["state"].tolist()
         self.create_state_marker()
@@ -35,8 +40,9 @@ class GeographyManager(Turtle):
         self.write(f"{state_name}", False, align=ALIGNMENT, font=(CLASS_FAMILY, 8, 'normal'))
 
     def create_csv_to_learn(self):
+        country_selected = self.country
         missing_states_dict = {"states remaining": self.column_state_list}
         missing_states = pandas.DataFrame(missing_states_dict)
-        missing_states.to_csv("public/data/states_to_learn.csv")
+        missing_states.to_csv(f"public/data/states_to_learn_{country_selected}.csv")
         return missing_states
 
--- main.py
+++ main.py
@@ -2,25 +2,35 @@ import turtle
 from assets.writerManager import GeographyManager
 
 
+print("Escolha um país para escrever os estados (Brasil ou USA): ")
+country_chosen = input().title()
 
-GeoManager = GeographyManager()
-usa_state_data = GeoManager.geographyDataFrame
+GeoManager = GeographyManager(country_chosen)
 
 #Criação da tela do jogo
 screen = turtle.Screen()
-screen.title("U.S. States Game")
-img_path = "public/img/blank_states_img.gif"
+screen.title("Country States")
+if country_chosen == "Usa":
+    img_path = "public/img/blank_states_img.gif"
+elif country_chosen == "Brasil":
+    img_path = "public/img/Brazil_states.gif"
+else:
+    img_path = "public/img/nenhum_pais_selecionado.gif"
+
+
 turtle.addshape(img_path)
 turtle.shape(img_path)
 
-
 GAME_IS_ON = True
-
 corrected_states = 0
 
+USER_PROMPT = "Type a state name that you remember:\nType Exit to leave"
+TITLE_INPUT = f"Corrected States: {corrected_states}/50"
+
+#Managing the game
 while GAME_IS_ON:
     states_guessed_write = []
-    answer_state = screen.textinput(title=f"Corrected States: {corrected_states}/50", prompt="Type a state name that you remember:\nType Exit to leave")
+    answer_state = screen.textinput(title=TITLE_INPUT, prompt=USER_PROMPT)
     answer_state = answer_state.title()
     print(answer_state)
     screen.update()
