b1b4aac8adcdfff9124a499f50654d3d5e655a40
henrySilverIX <henrysilver.gold@proton.me>
2025-04-26 16:27:20 -0300

    feat: Adicionei os países da Europa como um possível quiz. Também coloquei uma coluna a mais para os nomes dos estados em inglês e modifiquei uma coluna para deixar os nomes em português.

--- assets/writerManager.py
+++ assets/writerManager.py
@@ -11,16 +11,23 @@ CLASS_FAMILY = "Montserrat"
 
 
 class GeographyManager(Turtle):
-    def __init__(self, country):
+    def __init__(self, mapa):
         super().__init__()
-        self.country = country
-        if country == 'Usa':
+        self.mapa = mapa
+        if self.mapa == 'Usa' or self.mapa == "Eua":
             csv_path = os.path.join(base_path, '..', 'public', 'data', '50_states.csv')
-        elif country == "Brasil":
+        elif mapa == "Brasil" or self.mapa == "Brazil":
             csv_path = os.path.join(base_path, '..', 'public', 'data', '26_states_brazil.csv')
+        elif mapa == "Europa" or self.mapa == "Europe":
+            csv_path = os.path.join(base_path, '..', 'public', 'data', 'europe_countries.csv')
+        else:
+            csv_path = os.path.join(base_path, '..', 'public', 'data', 'europe_countries.csv')
 
         self.geographyDataFrame = pandas.read_csv(csv_path)
-        self.column_state_list = self.geographyDataFrame["state"].tolist()
+        self.pt_list = self.geographyDataFrame["state_name_pt"].tolist()
+        self.en_list = self.geographyDataFrame["state_name_en"].tolist()
+        self.column_state_list = self.pt_list + self.en_list
+        self.StatesFixedQTDE = int(len(self.column_state_list) / 2)
         self.create_state_marker()
 
     def create_state_marker(self):
@@ -32,17 +39,29 @@ class GeographyManager(Turtle):
 
     def write_state_on_map(self, state_name):
         #Guarda a informação de uma linha em uma variável
-        state_info_row = self.geographyDataFrame[self.geographyDataFrame["state"] == state_name]
-
+        state_info_row = self.geographyDataFrame[(self.geographyDataFrame["state_name_pt"] == state_name) | (self.geographyDataFrame["state_name_en"] == state_name)]
         coordinate_x = state_info_row.x.item()
         coordinate_y = state_info_row.y.item()
         self.goto(coordinate_x,coordinate_y)
         self.write(f"{state_name}", False, align=ALIGNMENT, font=(CLASS_FAMILY, 8, 'normal'))
 
+
+    def personalized_title(self, frase):
+        excecoes = {'e', 'de', 'do', 'da', 'em', 'and'}
+        palavras = frase.split()
+        resultado = []
+        for i, palavra in enumerate(palavras):
+            if palavra.lower() in excecoes and i != 0:
+                resultado.append(palavra.lower())
+            else:
+                resultado.append(palavra.capitalize())
+        return ' '.join(resultado)
+
+
     def create_csv_to_learn(self):
-        country_selected = self.country
+        mapa_selected = self.mapa
         missing_states_dict = {"states remaining": self.column_state_list}
         missing_states = pandas.DataFrame(missing_states_dict)
-        missing_states.to_csv(f"public/data/states_to_learn_{country_selected}.csv")
+        missing_states.to_csv(f"public/data/states_to_learn_{mapa_selected}.csv")
         return missing_states
 
--- main.py
+++ main.py
@@ -2,37 +2,46 @@ import turtle
 from assets.writerManager import GeographyManager
 
 
-print("Escolha um país para escrever os estados (Brasil ou USA): ")
-country_chosen = input().title()
+print("Escolha um mapa (Brasil, USA ou Europa): ")
+map_chosen = input().title()
+GeoManager = GeographyManager(map_chosen)
 
-GeoManager = GeographyManager(country_chosen)
 
 #Criação da tela do jogo
 screen = turtle.Screen()
 screen.title("Country States")
-if country_chosen == "Usa" or country_chosen == "Eua":
+screen.setup(width=750, height=750, startx=350, starty=10)
+if map_chosen == "Usa" or map_chosen == "Eua":
     img_path = "public/img/blank_states_img.gif"
-elif country_chosen == "Brasil":
+elif map_chosen == "Brasil" or map_chosen == "Brazil":
     img_path = "public/img/Brazil_states.gif"
+elif map_chosen == "Europa" or map_chosen == "Europe":
+    img_path = "public/img/Europa_countries.gif"
 else:
     img_path = "public/img/nenhum_pais_selecionado.gif"
-
-
 turtle.addshape(img_path)
 turtle.shape(img_path)
 
+
+
 GAME_IS_ON = True
 corrected_states = 0
+qtde_States_fixed = GeoManager.StatesFixedQTDE
 
+
+
+# def print_coord(x, y):
+#     print(f"{int(x)},{int(y)}")
+# screen.onscreenclick(print_coord)
 
 
 #Managing the game
 while GAME_IS_ON:
     USER_PROMPT = "Type a state name that you remember:\nType Exit to leave"
-    TITLE_INPUT = f"Corrected States: {corrected_states}/50"
+    TITLE_INPUT = f"Corrected States: {corrected_states}/{qtde_States_fixed}"
     states_guessed_write = []
     answer_state = screen.textinput(title=TITLE_INPUT, prompt=USER_PROMPT)
-    answer_state = answer_state.title()
+    answer_state = GeoManager.personalized_title(answer_state)
     print(answer_state)
     screen.update()
 
@@ -41,16 +50,30 @@ while GAME_IS_ON:
     if answer_state in GeoManager.column_state_list:
         GeoManager.write_state_on_map(answer_state)
         corrected_states += 1
-        GeoManager.column_state_list.remove(answer_state)
+        if answer_state in GeoManager.pt_list:
+            GeoManager.en_list.pop(GeoManager.pt_list.index(answer_state))
+            GeoManager.pt_list.remove(answer_state)
+            GeoManager.column_state_list.clear()
+            GeoManager.column_state_list = GeoManager.en_list + GeoManager.pt_list
+        else:
+            GeoManager.pt_list.pop(GeoManager.en_list.index(answer_state))
+            GeoManager.en_list.remove(answer_state)
+            GeoManager.column_state_list.clear()
+            GeoManager.column_state_list = GeoManager.en_list + GeoManager.pt_list
+
 
     #Count if the player answer all states correctly or if the player's typed exit to leave the game
     if corrected_states == 50 or not GeoManager.column_state_list or answer_state == "Exit":
         break
 
-    print(f"States Remaining: {len(GeoManager.column_state_list)}")
+    print(f"States Remaining: {int(len(GeoManager.column_state_list)/2)}")
 
 
+#Only creates a csv learn file if there's a missing states
+if len(GeoManager.column_state_list) != 0:
+    #Create a csv with the states that weren't typed
+    missing_states = GeoManager.create_csv_to_learn()
+    print(missing_states)
 
-#Create a csv with the states that weren't typed
-missing_states = GeoManager.create_csv_to_learn()
-print(missing_states)
+
+# turtle.mainloop()
